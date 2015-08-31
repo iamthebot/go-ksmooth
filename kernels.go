4ed0fb7ac89969a4c22bdad9ef787704bf316270
Alfredo Luque <alfredo@blacklab.capital>
2015-08-31 17:59:26 +0000

    add single precision support

--- kernels.go
+++ kernels.go
@@ -5,7 +5,12 @@ import (
 )
 
 var __OSQRT2PI__ float64 = 0.3989422804014326779399460599343818684758586311649346
+var __OSQRT2PISP__ float32 = 0.3989422
 
 func KernelGaussian(x float64, s float64) float64 {
 	return __OSQRT2PI__ * (1.0 / s) * float64(math.Exp(-0.5*(math.Pow(float64(x), 2.0)/math.Pow(float64(s), 2.0))))
+}
+
+func KernelGaussianSP(x float32, s float32) float32 {
+	return __OSQRT2PISP__ * (1.0 / s) * float32(math.Exp(-0.5*(math.Pow(float64(x), 2.0)/math.Pow(float64(s), 2.0))))
 }
--- smoother.go
+++ smoother.go
@@ -6,16 +6,16 @@ import (
 	"math"
 )
 
-type NWSmoother struct {
+type NWDPSmoother struct {
 	Bandwidth float64
 	Radius    int
 	Kernel    func(float64, float64) float64
 }
 
 //Allocates a new Nadraya-Watson kernel smoother using gaussian kernel
-func NewNWGaussianSmoother(bandwidth float64, density float64) (*NWSmoother, error) {
+func NewNWDPGaussianSmoother(bandwidth float64, density float64) (*NWDPSmoother, error) {
 	//Create NWSmoother
-	s := NWSmoother{
+	s := NWDPSmoother{
 		Bandwidth: bandwidth,
 		Kernel:    KernelGaussian,
 	}
@@ -32,7 +32,7 @@ func NewNWGaussianSmoother(bandwidth float64, density float64) (*NWSmoother, err
 //x is the x value which we want smoothed
 //inputs is the slice of y values.
 //the length we should assume for the input array
-func (s NWSmoother) SmoothPoint(x int, inputs []float64, length int) (float64, error) {
+func (s NWDPSmoother) SmoothPoint(x int, inputs []float64, length int) (float64, error) {
 	if x < 0 {
 		return 0.0, errors.New("x is out of range")
 	} else if length <= 0 {
@@ -53,6 +53,60 @@ func (s NWSmoother) SmoothPoint(x int, inputs []float64, length int) (float64, e
 	denSum := 0.0
 	for i := low; i <= high; i++ {
 		weight := s.Kernel(float64(i-x), s.Bandwidth/2.0)
+		numSum += weight * inputs[i]
+		denSum += weight
+	}
+
+	return numSum / denSum, nil
+}
+
+type NWSPSmoother struct {
+	Bandwidth float32
+	Radius    int
+	Kernel    func(float32, float32) float32
+}
+
+//Allocates a new Nadraya-Watson kernel smoother using gaussian kernel
+func NewNWSPGaussianSmoother(bandwidth float32, density float32) (*NWSPSmoother, error) {
+	//Create NWSmoother
+	s := NWSPSmoother{
+		Bandwidth: bandwidth,
+		Kernel:    KernelGaussianSP,
+	}
+
+	//Calculate Radius
+	//we use density as 2-tailed probability in InverseNormalCDF
+	tail := (1.0 - density) / 2.0
+	s.Radius = int(math.Max(float64(stat.NormalInv_CDF(float64(1.0-tail), float64(bandwidth/2.0))), 1.0))
+
+	return &s, nil
+}
+
+//Computes Nadraya-Watson regression at given point
+//x is the x value which we want smoothed
+//inputs is the slice of y values.
+//the length we should assume for the input array
+func (s NWSPSmoother) SmoothPoint(x int, inputs []float32, length int) (float32, error) {
+	if x < 0 {
+		return 0.0, errors.New("x is out of range")
+	} else if length <= 0 {
+		return 0.0, errors.New("length must be a positive integer")
+	}
+
+	low := 0
+	if x-s.Radius > low {
+		low = x - s.Radius
+	}
+	high := length - 1
+
+	if x+s.Radius/2 < high {
+		high = x + s.Radius/2
+	}
+
+	numSum := float32(0.0)
+	denSum := float32(0.0)
+	for i := low; i <= high; i++ {
+		weight := s.Kernel(float32(i-x), s.Bandwidth/2.0)
 		numSum += weight * inputs[i]
 		denSum += weight
 	}
